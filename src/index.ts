bd52b988a97fe6d498b4bea35c6b8caccb52b113
Selçuk Çukur <selcukcukur@outlook.com.tr>
2026-09-18 14:52:04 +0000

    feat: expose public CVX API

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+export { cn, twJoin, twMerge } from "./cn";
+export { cx } from "./class-value";
+export { configure, cv } from "./core";
+export type {
+  AnyCX,
+  ClassArray,
+  ClassDictionary,
+  ClassProp,
+  ClassValue,
+  CompoundVariant,
+  Configure,
+  ConfigureOptions,
+  CV,
+  CVComponent,
+  CVComponentShape,
+  CVConfig,
+  CX,
+  CXInput,
+  InternalVariantKey,
+  VariantProps,
+  VariantSelection,
+  VariantShape,
+} from "./types";
+export type {
+  ClassNameArray,
+  ClassNameValue,
+  CnFunction,
+  Engine as MergeEngine,
+  EngineOptions as MergeEngineOptions,
+} from "./merge/types";
